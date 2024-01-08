7322c153b665798fec5e97ad58da6e06627246af
HuaHuaChiChi <154486286+HuaHuaChiChi@users.noreply.github.com>
2024-01-08 11:57:28 +0900

    [level unrated] Title: 배열의 평균값, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.js
@@ -0,0 +1,8 @@
+function solution(numbers) {
+    var ans = 0;
+    let num = numbers.length
+    for (let i = 0; i < num; i++) {
+        ans += numbers[i];
+    }
+    return answer = ans / num
+}
